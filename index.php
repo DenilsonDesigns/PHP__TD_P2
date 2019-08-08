3a78316937d768ecaad79033f85c625128b10050
DenilsonDesigns <daniellanguilller@gmail.com>
2019-08-08 21:48:53 +1000

    second commit

--- index.php
+++ index.php
@@ -27,6 +27,15 @@ if (isset($_POST['wrong'])) {
     $_SESSION['question_no']++;
     $_SESSION['wrongos']++;
 }
+
+if ($_SESSION['question_no'] < $_SESSION['total_qs'] + 1) {
+
+    $answer = $questions[$_SESSION['question_no'] - 1]['correctAnswer'];
+    $first_wrong = $questions[$_SESSION['question_no'] - 1]['firstIncorrectAnswer'];
+    $second_wrong = $questions[$_SESSION['question_no'] - 1]['secondIncorrectAnswer'];
+    $first_adder = $questions[$_SESSION['question_no'] - 1]['leftAdder'];
+    $second_adder = $questions[$_SESSION['question_no'] - 1]['rightAdder'];
+}
 ?>
 
 <!DOCTYPE html>
@@ -41,19 +50,33 @@ if (isset($_POST['wrong'])) {
 </head>
 
 <body>
-    <?php echo "Session q no: " . $_SESSION['question_no']; ?>
+    <!-- <?php echo "Session q no: " . $_SESSION['question_no']; ?> -->
+    <?php if ($_SESSION['question_no'] < $_SESSION['total_qs'] + 1) {
+        echo "Answer: " . $answer;
+    } ?>
     <div class="container">
         <div id="quiz-box">
-            <p class="breadcrumbs">Question <?php echo $_SESSION['question_no']; ?>
-                of <?php echo $_SESSION['total_qs']; ?></p>
-            <p class="quiz">What is 54 + 71?</p>
-            <form action="index.php" method="POST">
-                <input type="hidden" name="id" value="0" />
-                <input type="submit" class="btn" name="wrong" value="135" />
-                <input type="submit" class="btn" name="correct" value="125" />
-                <input type="submit" class="btn" name="wrong" value="115" />
-                <input type="submit" class="btn" name="refresh" value="Start Again?" />
+            <?php if ($_SESSION['question_no'] < $_SESSION['total_qs'] + 1) {
+                echo '<p class="breadcrumbs">Question ' . $_SESSION['question_no'] . '
+                of ' . $_SESSION['total_qs'] . '</p>';
+            } ?>
 
+            <?php if ($_SESSION['question_no'] < $_SESSION['total_qs'] + 1) {
+                echo   '<p class="quiz">What is <?php echo $first_adder; ?> + <?php echo $second_adder; ?>?</p>';
+            } ?>
+            <form action="index.php" method="POST">
+                <?php
+                if ($_SESSION['question_no'] < $_SESSION['total_qs'] + 1) {
+                    echo '<input type="hidden" name="id" value="0" />
+                    <input type="submit" class="btn" name="wrong" value="' . $first_wrong . '" />
+                    <input type="submit" class="btn" name="correct" value="' . $answer . '" />
+                    <input type="submit" class="btn" name="wrong" value="' . $second_wrong . '" />';
+                } else {
+                    echo '<p>Game Over!</p>';
+                    echo '<p>Your score: ' . $_SESSION['correctos'] . '</p>';
+                }
+                ?>
+                <input type="submit" class="btn" name="refresh" value="Start Again?" />
             </form>
             <?php
             if (isset($_POST['correct'])) {
