bcb84f67384edc278423c6bf58496c09fef302c7
DenilsonDesigns <daniellanguilller@gmail.com>
2019-08-10 10:21:54 +1000

    added js file

---
+++ inc/answer.js
@@ -0,0 +1,10 @@
+// alert("connected");
+
+function toggleFunc() {
+  var x = document.getElementById("answer_p");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
